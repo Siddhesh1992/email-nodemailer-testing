9e296f86507d1ef7225961eb3c34e18ed4a4d09a
Siddhesh1992 <frostburn619@gmail.com>
2024-01-24 14:47:52 +0530

    changes

--- index.js
+++ index.js
@@ -5,32 +5,59 @@ const app = express();
 require("./db");
 const EmailTemplate = require("./emailSchema");
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.ethereal.email",
+//   port: 587,
+//   auth: {
+//     user: "brandt.leannon@ethereal.email",
+//     pass: "AsqJ3qRbVCUmddYgG4",
+//   },
+// });
+const from = "nya.schmitt84@ethereal.email";
 const transporter = nodemailer.createTransport({
   host: "smtp.ethereal.email",
   port: 587,
   auth: {
-    user: "brandt.leannon@ethereal.email",
-    pass: "AsqJ3qRbVCUmddYgG4",
+    user: "nya.schmitt84@ethereal.email",
+    pass: "V1AeK61rfMU77ptPz8",
   },
 });
 
-app.get("/update", async (req, res) => {
+app.get("/test", async (req, res) => {
   const templates = await EmailTemplate.find({
-    notificationName: "COMPANY_OFFBOARDING_UAM_NOTIFICATION",
+    email_Message: /<img alt="[Insert alt text for header image here]" class="header-img" height="120" src="{{HEADER_IMAGE}}" style="width:100%;height:120px;" width="100%" \/>/,
   });
 
-  res.send(
-    templates[0].email_Message.replace(
-      '<img alt="[Insert alt text for header image here]" class="header-img" height="100%" src="{{HEADER_IMAGE}}" style="width:100%;height:100%" width="100%">',
-      "<div>Testing</div>"
-    )
-  );
+  res.send(templates);
+});
+
+app.get("/update", async (req, res) => {
+  const templates = await EmailTemplate.find({});
+
+  const arr = [];
+  for (let temp of templates) {
+    if (
+      temp.email_Message.indexOf(
+        '<img alt="[Insert alt text for header image here]" class="header-img" height="100%" src="{{HEADER_IMAGE}}" style="width:100%;height:100%" width="100%">'
+      ) != -1
+    ) {
+      temp.email_Message = temp.email_Message.replace(
+        '<img alt="[Insert alt text for header image here]" class="header-img" height="100%" src="{{HEADER_IMAGE}}" style="width:100%;height:100%" width="100%">',
+        '<img alt="[Insert alt text for header image here]" class="header-img" height="120" src="{{HEADER_IMAGE}}" style="width:100%;height:120px;" width="100%" />'
+      );
+
+      arr.push(temp);
+      const t = await temp.save();
+    }
+  }
+
+  res.send("test" + arr.length);
 });
 
 app.get("/", async (req, res) => {
   try {
     const templates = await EmailTemplate.find({
-      notificationName: "COMPANY_OFFBOARDING_UAM_NOTIFICATION",
+      notificationName: "COMPANY_REACTIVATION_NOTIFICATION",
     });
 
     const img = fs.readFileSync("./emailBanner.png");
@@ -39,11 +66,13 @@ app.get("/", async (req, res) => {
       "data:image/png;base64," + Buffer.from(img).toString("base64");
     for (let temps of templates) {
       const info = await transporter.sendMail({
-        from: "meredith24@ethereal.email",
+        from,
         to: "frostburn619@gmail.com",
         subject: temps?.email_Subject,
         html: temps?.email_Message.replace("{{HEADER_IMAGE}}", banner),
       });
+
+      console.log(info);
     }
     res.send(templates[0]?.email_Message.replace("{{HEADER_IMAGE}}", banner));
   } catch (e) {
@@ -51,6 +80,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("server connected");
 });
